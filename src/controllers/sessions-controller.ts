d74b138b40ea6c88f377c1904fd65b6806075851
Leonardo Silva <leonardosilvalima93@live.com>
2025-02-05 16:41:41 -0300

    Token do Usuário

--- src/controllers/sessions-controller.ts
+++ src/controllers/sessions-controller.ts
@@ -1,8 +1,10 @@
 import { AppError } from '@/utils/AppError';
 import { Request, Response } from 'express';
 import { prisma } from '@/database/prisma';
-import { z } from 'zod';
+import { authConfig } from '@/configs/auth';
+import { sign } from 'jsonwebtoken';
 import { compare } from 'bcrypt';
+import { z } from 'zod';
 
 class SessionsController {
   async create(request: Request, response: Response) {
@@ -27,7 +29,14 @@ class SessionsController {
       throw new AppError('Invalid email or password', 401);
     }
 
-    return response.json({ message: 'ok' });
+    const { secret, expiresIn } = authConfig.jwt
+
+    const token = sign({ role: user.role ?? "customer" }, secret, {
+      subject: user.id,
+      expiresIn
+    })
+
+    return response.json({ token });
   }
 }
 
